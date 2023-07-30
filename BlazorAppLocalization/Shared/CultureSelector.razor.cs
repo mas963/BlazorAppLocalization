12f7c1cba4188dc9c69768cf7f407aa1a223e14e
Yasar Can <38841941+mas963@users.noreply.github.com>
2023-07-30 12:39:02 +0300

    first commit

---
+++ BlazorAppLocalization/Shared/CultureSelector.razor.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.JSInterop;
+using System.Globalization;
+
+namespace BlazorAppLocalization.Shared;
+
+public partial class CultureSelector
+{
+    [Inject]
+    public NavigationManager NavManager { get; set; }
+
+    [Inject]
+    public IJSRuntime JSRuntime { get; set; }
+
+    CultureInfo[] cultures = new[]
+    {
+        new CultureInfo("tr-TR"),
+        new CultureInfo("en-US"),
+    };
+
+    CultureInfo Culture
+    {
+        get => CultureInfo.CurrentCulture;
+        set
+        {
+            if (CultureInfo.CurrentCulture != value)
+            {
+                var js = (IJSInProcessRuntime)JSRuntime;
+                js.InvokeVoid("blazorCulture.set", value.Name);
+
+                NavManager.NavigateTo(NavManager.Uri, forceLoad: true);
+            }
+        }
+    }
+}
